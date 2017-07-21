c494d834fdd3ce3df1d74cca3a96966dc1a6f2f9
Thomas <thomas127@hotmail.com>
2017-07-21 22:56:06 +0200

    Get todos id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express'),
 	bodyParser = require('body-parser');
 
+
 const { mongoose } = require('./db/mongoose'),
 	{ Todo } = require('./models/todo'),
-	{ User } = require('./models/user');
+	{ User } = require('./models/user'),
+	{ ObjectId } = require('mongodb');
 
 const app = express();
 
@@ -34,7 +36,28 @@ app.get('/todos', (req, res) => {
 		res.status(400).send(e);
 	});
 
-})
+});
+
+// Get todo by ID
+app.get('/todos/:id', (req, res) => {
+	let id = req.params.id;
+
+	// validate id (404 - send back empty send)
+	if (!ObjectId.isValid(id)) return res.status(404).send('Id is not valid');
+
+	// find by id
+	Todo.findById(id).then( todo => {
+	
+		// if no todo send 404 with empty body
+		if (!todo) return res.status(404).send();
+
+		// if todo exist send it back
+		res.send({todo});
+
+		// on error send 400 with empty body
+	}).catch( e => res.status(400).send());
+
+});
 
 app.listen(3000, () => {
 	console.log('Started on port 3000.');
